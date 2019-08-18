d4ec1ce7e744f6763533748906511f4001e88444
Sajjon <alex.cyon@gmail.com>
2019-08-18 17:26:32 +0200

    Initial Commit

---
+++ SHAPerformanceEvaluation/SHAPerformanceEvaluation.h
@@ -0,0 +1,19 @@
+//
+//  SHAPerformanceEvaluation.h
+//  SHAPerformanceEvaluation
+//
+//  Created by Alexander Cyon on 2019-08-18.
+//  Copyright © 2019 Alex Cyon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SHAPerformanceEvaluation.
+FOUNDATION_EXPORT double SHAPerformanceEvaluationVersionNumber;
+
+//! Project version string for SHAPerformanceEvaluation.
+FOUNDATION_EXPORT const unsigned char SHAPerformanceEvaluationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHAPerformanceEvaluation/PublicHeader.h>
+
+
